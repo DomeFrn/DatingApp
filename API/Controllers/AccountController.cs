20f0555607266d467c732ea9c43bad431bfaee39
dominik.frenzel <dominik.frenzel@medifoxdan.de>
2024-08-26 12:02:56 +0200

    Toekns Add

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,16 +3,17 @@ using System.Security.Cryptography;
 using System.Text;
 using API.Data;
 using API.DTOs;
+using API.Interface;
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context) : BaseApiController
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")] // account/register
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Nutzername bereits vergeben");
 
@@ -28,11 +29,15 @@ public class AccountController(DataContext context) : BaseApiController
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
-        return user;
+        return new UserDto
+        {
+            Username = user.Username,
+            Token = tokenService.CreateToken(user)
+        };
     }
 
     [HttpPost("login")] //Http  Request für Anmeldung
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
         var user = await context.Users.FirstOrDefaultAsync(x => x.Username == loginDto.Username.ToLower()); // Sucht ob eingegebener Nutzer in DB existiert
 
@@ -47,7 +52,11 @@ public class AccountController(DataContext context) : BaseApiController
             if (ComputedHash[i] != user.PasswortHash[i]) return Unauthorized("Ungültiges Passwort");
         }
 
-        return user;
+         return new UserDto
+        {
+            Username = user.Username,
+            Token = tokenService.CreateToken(user)
+        };
     }
 
     // Methode zum Prüfen auf Doppelte Passwörter
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Interface;
+using API.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class TokenService(IConfiguration config) : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        var tokenKey = config["TokenKey"] ?? throw new Exception("Kann keinen TokenKey von appsetting annehmen");
+        if (tokenKey.Length < 64) throw new Exception("TokenKey muss länger sein");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.Username)
+        };
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
